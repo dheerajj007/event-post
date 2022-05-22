fa66b540b941da961db83c4267b181c61bdaef3a
aman043 <dheerajkumarr235@gmail.com>
2022-05-22 11:46:19 +0530

    added crud functianlity

---
+++ base/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post, Topic, Message
+
+admin.site.register(Post)
+admin.site.register(Topic)
+admin.site.register(Message)
---
+++ base/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('post/<str:pk>', views.post, name="post"),
+    path('create-post/', views.createPost, name="create-post"),
+    path('update-post/<str:pk>', views.updatePost, name="update-post"),
+    path('delete-post/<str:pk>', views.deletePost, name="delete-post"),
+]
